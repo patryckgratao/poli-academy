c2d7db107800210c4ab92c65f9ee7f52a27296eb
Patryck Gratão <patryck@polichat.com.br>
2021-02-18 09:29:41 -0300

    :sparkles: api service updated

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
 
-export default api;
+const getAllProducts = () => {
+  return axios.get(`http://localhost:3333/products`);
+}
+
+const getAllStock = () => {
+  return axios.get(`http://localhost:3333/stock`);
+}
+
+export default { getAllProducts, getAllStock };
